30e8c25152b193212659fd446d1b4a8378557091
newInDeve <interestincoding5@gmail.com>
2023-04-19 13:32:41 +0400

    Updated React Js Files

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 const SearchBar = (props) => {
   const [findLocation, setFindLocation] = useState("");
   const catchSavevalue = function (event) {
+    console.log(event.keyCode);
     if (
       event.key === "Enter" ||
       event.key === "Go" ||
@@ -12,17 +13,19 @@ const SearchBar = (props) => {
       event.keyCode === 13
     ) {
       event.preventDefault();
+      alert(event.target.value);
       props.handleSearch(event.target.value);
     }
   };
 
-  const handleKeyDown = (event) => {
-    if (event.keyCode === 13 || event.which === 13) {
-      event.preventDefault();
-      alert("Hey");
-      props.handleSearch(findLocation);
-    }
-  };
+  // const handleSubmit = () => {
+  //   // if (event.keyCode === 13 || event.which === 13) {
+  //   //   event.preventDefault();
+
+  //   // }
+  //   alert("Hey");
+  //   props.handleSearch(findLocation);
+  // };
 
   return (
     <motion.div
@@ -31,7 +34,6 @@ const SearchBar = (props) => {
       animate={{ width: `${35}%`, visibility: "visible" }}
       transition={{ type: "spring", delay: 0.5, duration: 3 }}
       onKeyUp={catchSavevalue}
-      onKeyDown={handleKeyDown}
     >
       <input
         type="text"
