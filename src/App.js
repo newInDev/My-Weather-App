92924a5a0a1cba8a2e24abe44312cf2484ed66d5
newInDeve <interestincoding5@gmail.com>
2023-04-19 17:39:15 +0400

    Updated React Js Files

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
 
   const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,relativehumidity_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&timeformat=unixtime&timezone=Europe%2FMoscow`;
 
-  const geoLocationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.React_App_Location_Api}`;
+  const geoLocationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${process.env.REACT_APP_API_KEY}`;
 
   let tempObject = [];
 
